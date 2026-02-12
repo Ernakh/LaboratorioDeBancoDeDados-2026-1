6bd7198655b21a0ff06dbc69446e1a32b16b5249
Fabrício <fabriciotonettolondero@gmail.com>
2026-02-12 10:38:39 -0300

    Update dinossauros.sql

--- dinossauros.sql
+++ dinossauros.sql
@@ -38,3 +38,76 @@ foreign key (fk_era) references eras(id),
 foreign key (fk_descobridor) references descobridores (id),
 foreign key (fk_regiao) references regioes(id)
 )
+
+
+
+
+create database dinossauros;
+
+create table grupos
+(
+id integer not null primary key identity,
+nome varchar(40) not null
+);
+
+create table descobridores 
+(
+id integer not null primary key identity,
+nome varchar(100) not null
+);
+
+create table regioes
+(
+id integer not null primary key identity,
+nome varchar(150) not null unique
+);
+
+create table eras 
+(
+id integer not null primary key identity,
+nome varchar(35) not null unique,
+inicio integer not null,
+fim integer not null
+)
+
+create table dinossauros(
+id integer not null primary key identity,
+nome varchar(150) not null unique,
+toneladas integer,inicio integer,
+fim integer,fk_grupo integer,
+fk_era integer,fk_descobridor integer,
+fk_regiao integer,
+foreign key (fk_grupo) references grupos (id),
+foreign key (fk_era) references eras(id),
+foreign key (fk_descobridor) references descobridores (id),
+foreign key (fk_regiao) references regioes(id)
+)
+
+
+insert into grupos values ('Anquilossauros')
+insert into grupos values ('Ceratopsídeos')
+insert into grupos values ('Estegossauros');
+insert into grupos values ('Terápodes');
+
+insert into eras values ('Cretáceo', 145, 65)
+insert into eras values ('Jurássico', 201, 145)
+
+insert into descobridores values ('Maryanska');
+insert into descobridores values ('John Bell Hatcher');
+insert into descobridores values ('Cientistas Alemães');
+insert into descobridores values ('Museu Americano de História Natural');
+insert into descobridores values ('Othniel Charles Marsh');
+insert into descobridores values ('Barn Brown');
+
+insert into regioes values ('Mongólia');
+insert into regioes values ('Canadá');
+insert into regioes values ('China');
+
+insert into dinossauros 
+values ('Saichania', 2,145,66, 1, 2,1,1)
+
+select * from grupos
+select * from regioes
+select * from descobridores
+select * from eras
+select * from dinossauros
