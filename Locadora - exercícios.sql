831dcd5b336c49d927a2a575d3de0bb7b0d1c8eb
Fabrício <fabriciotonettolondero@gmail.com>
2026-02-19 09:50:32 -0300

    Create Locadora - exercícios.sql

---
+++ Locadora - exercícios.sql
@@ -0,0 +1,11 @@
+--Elaborar o Script de criação do BD Locadora com todas suas tabelas, atributos, PKs, FKs e com no mínimo 10 inserts em cada tabela (20 nas associativas).
+--Elabore as seguintes consultas:
+-- - Que liste todos os autores e os filmes que atuaram.
+-- - Que relacione todos os clientes com os filmes locados.
+-- - Que mostre todos os filmes e seus generos e categorias.
+-- - Que liste os filmes locados em 2026.
+-- - Que mostre os 2 filmes mais locados no ano de 2025.
+-- - Que mostre o Gênero mais locado.
+-- - Que conte os clientes por estados.
+
+
