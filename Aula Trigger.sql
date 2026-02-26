fa9a150f49923b584955a719484522626659908d
Fabrício <fabriciotonettolondero@gmail.com>
2026-02-26 08:36:12 -0300

    Create trigger to log professor deletions
    Add a PostgreSQL trigger function to log deletions of professors.

--- Aula Trigger.sql
+++ Aula Trigger.sql
@@ -29,3 +29,18 @@ begin
 	insert into log values ('exclusão', 
 	'foi excluido um professor em java', GETDATE());
 end
+
+
+--primeira trigger POSTGRESQL
+CREATE OR REPLACE FUNCTION primeiratrigger() RETURNS trigger as $BODY$
+BEGIN
+	insert into log values ('exclusão', 'foi excluido um professor da academia java', TIMESTAMP;
+RETURN NEW;
+END;
+$BODY$
+LANGUAGE PLPGSQL VOLATILE;
+
+CREATE  TRIGGER t_primeiratrigger BEFORE DELETE ON JAVA
+FOR EACH ROW EXECUTE PROCEDURE primeiratrigger();
+
+
